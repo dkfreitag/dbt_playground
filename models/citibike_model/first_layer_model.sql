92d9438aacd425cd49768572b3e03264a1015173
David Freitag <dkfreitag@gmail.com>
2026-05-22 23:40:25 +0000

    formatting

--- models/citibike_model/first_layer_model.sql
+++ models/citibike_model/first_layer_model.sql
@@ -1,11 +1,17 @@
-with raw_bike_trips as (
+with
+    raw_bike_trips as (
 
-    select bikeid, starttime, stoptime, start_station_id, end_station_id, start_station_name, end_station_name
-    from `bone-and-lumber-12345.dbt_dfreitag.citibike_trips`
-    where bikeid is not null
-      and starttime is not null
-      and stoptime is not null
-)
+        select
+            bikeid,
+            starttime,
+            stoptime,
+            start_station_id,
+            end_station_id,
+            start_station_name,
+            end_station_name
+        from `bone-and-lumber-12345.dbt_dfreitag.citibike_trips`
+        where bikeid is not null and starttime is not null and stoptime is not null
+    )
 
 select *
 from raw_bike_trips
--- models/citibike_model/second_layer_model.sql
+++ models/citibike_model/second_layer_model.sql
@@ -1,9 +1,6 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-select
-     bikeid
-    ,stoptime -  starttime AS trip_time
-from {{ ref('first_layer_model') }}
-where starttime < DATE('2026-01-01')
-  and start_station_id = 3496 -- 1st ave and 110th st
+select bikeid, stoptime - starttime as trip_time
+from {{ ref("first_layer_model") }}
+where starttime < date('2026-01-01') and start_station_id = 3496  -- 1st ave and 110th st
 order by 2 desc
